9bbb030f20ab6e2b51bada2c313e96b8922ec061
Даниил <Grawl@ya.ru>
2013-01-31 00:28:31 +1100

    replace border with box-shadow
    instead of border that changes size of box I suggest outline, it's better. and box-shadow for appearance.

--- js/inplaceeditor.js
+++ js/inplaceeditor.js
@@ -122,7 +122,7 @@ $(document).ready(function(){
         if ($("script[src*='beautify-html.js']").length == 0)
             $('body').append('<script src="https://raw.github.com/einars/js-beautify/master/beautify-html.js"/>');
 
-        $("<style type='text/css'> .editablearea { outline:1px solid; } </style>").appendTo("head");
+        $("<style type='text/css'> .editablearea { box-shadow: 0 0 10px hsl(212, 80%, 50%); outline: 1px solid hsla(206, 77%, 61%, 0.3); } </style>").appendTo("head");
     }
 
     var addControls = function () {
