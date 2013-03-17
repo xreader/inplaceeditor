100ef548cfe9b7cab687b7eaa211260b7c17eed1
xreader <xreader@gmail.com>
2013-03-17 22:17:08 +0100

    Update inplaceeditor.js

--- js/inplaceeditor.js
+++ js/inplaceeditor.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
     var highlighting = true;
 
     //modes PHP, NODE, DEMO
-    var SERVER = 'NODE';
+    var SERVER = 'DEMO';
     var pathPrefix = SERVER === 'PHP' ? SERVER_PATH + 'server.php?action=' : '/';
 
     var savePath = pathPrefix + 'save';
